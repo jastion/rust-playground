ff36cb568b5f8a6a1df5a16fb8883485c10c9842
Michael Chiou <michaelchiu300@gmail.com>
2021-03-16 00:28:22 -0700

    WIP: including mod and buildable Rust Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod parser;
+
+//use csv::Error;
 // This is a comment, and is ignored by the compiler
 // You can test this code by clicking the "Run" button over there ->
 // or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
@@ -6,9 +9,8 @@
 // You can always return to the original code by clicking the "Reset" button ->
 
 // This is the main function
+
 fn main() {
     // Statements here are executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
+    parser::print_hello();
 }
